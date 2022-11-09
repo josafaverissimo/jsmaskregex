d3ba5490d0a368764d4e852b04d141ed606b59fe
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2022-11-09 19:36:59 -0300

    cpf mask finished

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const cpfMask = inputElement => {
     let startSelection = inputElement.selectionStart
-    let endSelection = inputElement.selectionEnd
-    
+    let endSelection = inputElement.selectionEnd    
+
     let inputValue = inputElement.value
+    const nextCharIndex = endSelection
+    const nextChar = inputValue[nextCharIndex]
+
     inputValue = inputValue.replace(/[^0-9]*/g, "")
     inputValue = inputValue.replace(/^(.{11}).*$/g, "$1")
     
     
     let inputNewValue = inputValue.replace(/([0-9]{3})([0-9]{3})?([0-9]{3})?([0-9]{2})?/g, "$1.$2.$3-$4")
     inputNewValue = inputNewValue.replace(/(?<=\.)\.-|(?<=[0-9]\.)-/g, "")
+
+    if(/[.-]/.test(nextChar)) {
+        startSelection++
+        endSelection++
+    }
 
     inputElement.value = inputNewValue
     inputElement.setSelectionRange(startSelection, endSelection)
