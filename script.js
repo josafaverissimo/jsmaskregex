ebb41f1598585e9175ca0f30db5425e02b8676ea
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2022-11-09 19:26:39 -0300

    refactor and bug fixes

--- script.js
+++ script.js
@@ -1,31 +1,18 @@
 const cpfMask = inputElement => {
-    console.log(inputElement.value)
-    inputElement.value = inputElement.value.replace(/[.-]/g, "")
-    inputElement.value = inputElement.value.replace(/^(.{11}).*$/g, "$1")
+    let startSelection = inputElement.selectionStart
+    let endSelection = inputElement.selectionEnd
     
-    const inputValue = inputElement.value
-    const lastElementRegex = /.$/
-    let lastCharTyped = inputValue.match(lastElementRegex)
+    let inputValue = inputElement.value
+    inputValue = inputValue.replace(/[^0-9]*/g, "")
+    inputValue = inputValue.replace(/^(.{11}).*$/g, "$1")
     
-    if(lastCharTyped != null) {
-        lastCharTyped = lastCharTyped[0]
-
-        
-        if(isNaN(Number(lastCharTyped))) {
-            const ignoreLastCharRegex = new RegExp('^(.{' + (inputValue.length - 1) + '}).*$', 'g')
-            const noLastChar = "$1"
-            let inputNewValue = inputValue.replace(ignoreLastCharRegex, noLastChar)
-
-            inputElement.value = inputNewValue
-
-            return
-        }
-    }
-
+    
     let inputNewValue = inputValue.replace(/([0-9]{3})([0-9]{3})?([0-9]{3})?([0-9]{2})?/g, "$1.$2.$3-$4")
     inputNewValue = inputNewValue.replace(/(?<=\.)\.-|(?<=[0-9]\.)-/g, "")
+
     inputElement.value = inputNewValue
+    inputElement.setSelectionRange(startSelection, endSelection)
 
 }
 
-document.getElementById("cpf").addEventListener("input", event => cpfMask(event.target))
+document.getElementById("cpf").addEventListener("input", ({ target }) => cpfMask(target))
