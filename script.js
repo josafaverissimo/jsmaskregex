035da43879f70232f119857a18714a6c43e1833b
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2022-11-10 16:25:59 -0300

    refactor(cpfMask): removing comments

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ const cpfMask = inputElement => {
     let inputNewValue = inputValue.replace(/([0-9]{3})([0-9]{3})?([0-9]{3})?([0-9]{2})?/g, "$1.$2.$3-$4")
     inputNewValue = inputNewValue.replace(/(?<=\.)\.-|(?<=[0-9]\.)-/g, "")
 
-    // if(currentCharIndex >= 3) {
+    if(currentCharIndex >= 3) {
         if(/^[0-9]{4}$/.test(lastSequence)) {
             startSelection++
             endSelection++
         }
-    // }
+    }
 
     inputElement.value = inputNewValue
     inputElement.setSelectionRange(startSelection, endSelection)
