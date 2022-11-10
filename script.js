44c8e36cd877740fc0f24b6b32ba2e25491b1b0c
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2022-11-10 18:49:40 -0300

    feat(phoneMask)|refactor(cpfMask):cpfMask function refactored and phoneMask now allowed remove characters

--- script.js
+++ script.js
@@ -1,53 +1,54 @@
 const cpfMask = inputElement => {
-    let startSelection = inputElement.selectionStart
-    let endSelection = inputElement.selectionEnd    
+    let cursorPosition = inputElement.selectionStart
 
     let inputValue = inputElement.value
-    const currentCharIndex = endSelection
-    const lastSequence = inputValue.slice(currentCharIndex - 4, currentCharIndex)
+    const currentCharIndex = cursorPosition - 1
+    const sequence = inputValue.slice(0, currentCharIndex + 1)
 
     if(inputValue.length > 14) {
-        if(startSelection == 1) {
-            inputValue = inputValue.slice(startSelection)
-        } else if(startSelection == 15) {
-            inputValue.slice(0, startSelection - 1)
+        if(cursorPosition == 1) {
+            inputValue = inputValue.slice(cursorPosition)
+        } else if(cursorPosition == 15) {
+            inputValue.slice(0, cursorPosition - 1)
         } else {
-            inputValue = inputValue.slice(0, startSelection - 1) + inputValue.slice(startSelection)
+            inputValue = inputValue.slice(0, cursorPosition - 1) + inputValue.slice(cursorPosition)
         }
 
-        startSelection--
-        endSelection--
+        cursorPosition--
     }
-
     
     inputValue = inputValue.replace(/[^0-9]*/g, "")
-
     inputValue = inputValue.replace(/^(.{11}).*$/g, "$1")
     
     
     let inputNewValue = inputValue.replace(/([0-9]{3})([0-9]{3})?([0-9]{3})?([0-9]{2})?/g, "$1.$2.$3-$4")
     inputNewValue = inputNewValue.replace(/(?<=\.)\.-|(?<=[0-9]\.)-/g, "")
 
-    if(currentCharIndex >= 3) {
-        if(/^[0-9]{4}$/.test(lastSequence)) {
-            startSelection++
-            endSelection++
-        }
+    if(/[0-9]{4}$/.test(sequence)) {
+        cursorPosition++
     }
 
     inputElement.value = inputNewValue
-    inputElement.setSelectionRange(startSelection, endSelection)
+    inputElement.setSelectionRange(cursorPosition, cursorPosition)
 }
 
-// (xx) 9 xxxx-xxxx
 const phoneMask = inputElement => {
+    let cursorPosition = inputElement.selectionStart
+
     const inputValue = inputElement.value
+    const currentCharIndex = cursorPosition - 1
+    const sequence = inputValue.slice(0, currentCharIndex + 1)
     let inputNewValue = inputValue.replace(/[^0-9]/g, "").replace(/^([0-9]{11}).*$/g, "$1")
 
     inputNewValue = inputNewValue.replace(/^(.)(.)?(.)?(....)?(....)?/, "($1$2) $3 $4-$5")
     inputNewValue = inputNewValue.replace(/(?<=\(.)\)  -|(?<=\(..\) ) -|(?<=\(..\) . )-/, "")
 
+    if(/^.$|^\([0-9]{3}$|^\(..\).$|^\(..\) ..$|^\(..\) . .{5}/g.test(sequence)) {
+        cursorPosition += 2
+    }
+
     inputElement.value = inputNewValue
+    inputElement.setSelectionRange(cursorPosition, cursorPosition)
 }
 
 
