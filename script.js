c10e9b95125b62a451efc9e199b916ced207097a
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2022-11-12 15:00:30 -0300

    refactor(cpfMask|phoneMask): cursor dont move when deleting input data

--- script.js
+++ script.js
@@ -2,31 +2,37 @@ const cpfMask = inputElement => {
     let cursorPosition = inputElement.selectionStart
 
     let inputValue = inputElement.value
-    const currentCharIndex = cursorPosition - 1
-    const sequence = inputValue.slice(0, currentCharIndex + 1)
 
     if(inputValue.length > 14) {
-        if(cursorPosition == 1) {
+        const initPosition = 1
+        const maxPosition = 15
+
+        if(cursorPosition == initPosition) {
             inputValue = inputValue.slice(cursorPosition)
-        } else if(cursorPosition == 15) {
+
+        } else if(cursorPosition == maxPosition) {
             inputValue.slice(0, cursorPosition - 1)
+
         } else {
             inputValue = inputValue.slice(0, cursorPosition - 1) + inputValue.slice(cursorPosition)
+
         }
 
         cursorPosition--
     }
     
-    inputValue = inputValue.replace(/[^0-9]*/g, "")
-    inputValue = inputValue.replace(/^(.{11}).*$/g, "$1")
-    
-    
-    let inputNewValue = inputValue.replace(/([0-9]{3})([0-9]{3})?([0-9]{3})?([0-9]{2})?/g, "$1.$2.$3-$4")
+    let inputNewValue = inputValue.replace(/[^0-9]*/g, "").replace(/^(.{11}).*$/g, "$1")
+    inputNewValue = inputNewValue.replace(/([0-9]{3})([0-9]{3})?([0-9]{3})?([0-9]{2})?/g, "$1.$2.$3-$4")
     inputNewValue = inputNewValue.replace(/(?<=\.)\.-|(?<=[0-9]\.)-/g, "")
+    const lastTwoChar = inputNewValue.slice(-2)
 
-    if(/[0-9]{4}$/.test(sequence)) {
-        cursorPosition++
+    if(/[.-]/.test(lastTwoChar)) {
+        const userIsDeleting = cpfMask.lastValue !== undefined ? cpfMask.lastValue.length >= inputValue.length : false
+
+        cursorPosition = userIsDeleting ? cursorPosition : cursorPosition + 1
     }
+
+    cpfMask.lastValue = inputValue
 
     inputElement.value = inputNewValue
     inputElement.setSelectionRange(cursorPosition, cursorPosition)
@@ -35,22 +41,39 @@ const cpfMask = inputElement => {
 const phoneMask = inputElement => {
     let cursorPosition = inputElement.selectionStart
 
-    const inputValue = inputElement.value
-    const currentCharIndex = cursorPosition - 1
-    const sequence = inputValue.slice(0, currentCharIndex + 1)
-    let inputNewValue = inputValue.replace(/[^0-9]/g, "").replace(/^([0-9]{11}).*$/g, "$1")
+    let inputValue = inputElement.value
 
+    if(inputValue.length > 16) {
+        if(cursorPosition == 1) {
+            inputValue = inputValue.slice(cursorPosition)
+        } else if(cursorPosition == 17) {
+            inputValue.slice(0, cursorPosition - 1)
+        } else {
+            inputValue = inputValue.slice(0, cursorPosition - 1) + inputValue.slice(cursorPosition)
+        }
+
+        cursorPosition--
+    }
+
+    let inputNewValue = inputValue.replace(/[^0-9]/g, "").replace(/^([0-9]{11}).*$/g, "$1")
     inputNewValue = inputNewValue.replace(/^(.)(.)?(.)?(....)?(....)?/, "($1$2) $3 $4-$5")
     inputNewValue = inputNewValue.replace(/(?<=\(.)\)  -|(?<=\(..\) ) -|(?<=\(..\) . )-/, "")
+    const lastTwoChar = inputNewValue.slice(-2)
 
-    if(/^[0-9]$|^\([0-9]{2}$|^\([0-9]{2}\)[0-9] $|^\([0-9]{2}\) [0-9]{2} $|^\([0-9]{2}\) [0-9] [0-9]{5}-$/.test(inputValue)) {
-        cursorPosition++
+    if(/[ ()-]/.test(lastTwoChar)) {
+        const userIsDeleting = phoneMask.lastValue !== undefined ? phoneMask.lastValue.length >= inputValue.length : false
+        
+        cursorPosition = userIsDeleting ? cursorPosition : cursorPosition + 1
     }
+
+    phoneMask.lastValue = inputValue
 
     inputElement.value = inputNewValue
     inputElement.setSelectionRange(cursorPosition, cursorPosition)
 }
 
 
-document.getElementById("phone").addEventListener("input", ({ target }) => phoneMask(target))
-document.getElementById("cpf").addEventListener("input", ({ target }) => cpfMask(target))
+document.getElementById("phone")
+    .addEventListener("input", ({ target }) => phoneMask(target))
+document.getElementById("cpf")
+    .addEventListener("input", ({ target }) => cpfMask(target))
