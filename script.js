1ba5d8e7c1456f4352ebaae90f53aa0098711aa8
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-08-18 09:33:11 -0300

    feat(index.html): phone and generic-mask added

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const cpfMask = inputElement => {
         const initPosition = 1
         const maxPosition = 15
 
-        if(cursorPosition == initPosition) {
+        if(cursorPosition === initPosition) {
             inputValue = inputValue.slice(cursorPosition)
 
-        } else if(cursorPosition == maxPosition) {
+        } else if(cursorPosition === maxPosition) {
             inputValue.slice(0, cursorPosition - 1)
 
         } else {
@@ -39,6 +39,7 @@ const cpfMask = inputElement => {
 }
 
 const phoneMask = inputElement => {
+    const mask = "(xx) {x} xxxx-xxxx"
     let cursorPosition = inputElement.selectionStart
 
     let inputValue = inputElement.value
@@ -56,14 +57,15 @@ const phoneMask = inputElement => {
     }
 
     let inputNewValue = inputValue.replace(/[^0-9]/g, "").replace(/^([0-9]{11}).*$/g, "$1")
-    inputNewValue = inputNewValue.replace(/^(.)(.)?(.)?(....)?(....)?/, "($1$2) $3 $4-$5")
-    inputNewValue = inputNewValue.replace(/(?<=\(.)\)  -|(?<=\(..\) ) -|(?<=\(..\) . )-/, "")
-    const lastTwoChar = inputNewValue.slice(-2)
-
-    if(/[ ()-]/.test(lastTwoChar)) {
-        const userIsDeleting = phoneMask.lastValue !== undefined ? phoneMask.lastValue.length >= inputValue.length : false
-        
-        cursorPosition = userIsDeleting ? cursorPosition : cursorPosition + 1
+    inputNewValue = inputNewValue
+        .replace(/^(.)(.)?(.{1,4})?(.{1,4})?/, "($1$2) 9 $3-$4")
+        .replace(/(?<=\(.)\) 9 -|(?<=\(..\)) 9 -/, "")
+    
+    const userIsDeleting = phoneMask.lastValue !== undefined ? phoneMask.lastValue.length >= inputValue.length : false
+    if(!userIsDeleting) {
+        while([1, 3, 4, 5, 6, 11].indexOf(cursorPosition) !== -1) {
+            cursorPosition++
+        }
     }
 
     phoneMask.lastValue = inputValue
@@ -75,5 +77,9 @@ const phoneMask = inputElement => {
 
 document.getElementById("phone")
     .addEventListener("input", ({ target }) => phoneMask(target))
+
 document.getElementById("cpf")
     .addEventListener("input", ({ target }) => cpfMask(target))
+
+document.getElementById("generic-mask")
+    .addEventListener("input", ({ target }) => genericMask(target))
